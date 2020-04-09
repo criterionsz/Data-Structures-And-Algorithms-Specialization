c75fb5949c8b9943b55fc68f68a58b77580fa02d
criterionsz <ed371612>
2020-04-09 12:26:57 +0300

    FibonacciPartialSum realisation

--- src/AlgorithmicToolbox/Week2/FibonacciPartialSum.kt
+++ src/AlgorithmicToolbox/Week2/FibonacciPartialSum.kt
@@ -1,27 +1,38 @@
 import java.util.*
 
-fun getFibonacciPartialSumNaive(from: Long, to: Long): Long {
-    var sum: Long = 0
-
-    var current: Long = 0
-    var next: Long = 1
-
-    for (i in 0..to) {
-        if (i >= from) {
-            sum += current
-        }
-
-        val newCurrent = next
-        next += current
-        current = newCurrent
+fun getFibonacciSumFast2(n: Long, t: Int): Long{
+    if (n%60 <= 1) {
+        if(t == 1)
+            return 0
+        else
+            return n%60
     }
-
-    return sum % 10
+    var previous = 0L
+    var current = 1L
+    var c = 1L
+    var sum = 1L
+    for(i in 1 until (n%60 - t)){
+        c = (previous+current)%10
+        previous = current
+        current = c
+        sum = (sum+ c)%10
+    }
+    return sum
 }
-
+fun getFibonacciPartialSumFast(from: Long, to: Long): Long{
+    if (from == 0L && to == 0L)
+        return 0
+    var sumFrom = getFibonacciSumFast2(from, 1)
+    var sumTo = getFibonacciSumFast2(to, 0)
+    if (sumTo<sumFrom){
+        sumTo+=10
+        return sumTo - sumFrom
+    }
+    return sumTo - sumFrom
+}
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
     val from = scanner.nextLong()
     val to = scanner.nextLong()
-    println(getFibonacciPartialSumNaive(from, to))
+    println(getFibonacciPartialSumFast(from, to))
 }
